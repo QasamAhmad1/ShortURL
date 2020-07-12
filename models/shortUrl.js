01f84d83e94405737252a6e37eb32454b30d21dc
Qasam Ahmad <qasam.ahmad@protonmail.ch>
2020-07-13 00:16:28 +0100

    added encode support

--- models/shortUrl.js
+++ models/shortUrl.js
@@ -1,6 +1,23 @@
 const mongoose = require('mongoose');
 const shortId = require('shortid');
 
+let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split("");
+let base = alphabet.length;
+
+//possible future change in shortUrl generation
+exports.encode = function(i) {
+  var s;
+  if (i === 0) {
+    return alphabet[0];
+  }
+  s = "";
+  while (i > 0) {
+    s += alphabet[i % base];
+    i = parseInt(i / base, 10);
+  }
+  return s.split("").reverse().join("");
+};
+
 const shortUrlSchema = new mongoose.Schema({
     full: {
         type: String,
